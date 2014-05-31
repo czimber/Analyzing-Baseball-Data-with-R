367266a6877aa512affaa46218d28c11f58fff53
czimber <craigzimber@gmail.com>
2014-05-31 14:56:22 -0700

    compeleted

--- Ch03.R
+++ Ch03.R
@@ -9,16 +9,27 @@
 ##############################################
 
 # Section 3.1 Introduction
-
 hof <- read.csv("hofbatting.csv")
+        summary(hof)
+        str(hof)
+        names(hof)
 
+# Section 3.2 Factor Variable
+# Section 3.2.1 A bar graph
+
+# Find mid-career
 hof$MidCareer <- with(hof, (From + To) / 2)
+        hof$MidCareer
 
+# Create Era
 hof$Era <- cut(hof$MidCareer,
                breaks = c(1800, 1900, 1919, 1941, 1960, 1976, 1993, 2050),
-               labels = c("19th Century", "Lively Ball", "Dead Ball",
+               labels = c("19th Century", "Dead Ball", "Lively Ball",
                           "Integration", "Expansion", "Free Agency", 
                           "Long Ball"))
+        summary(hof$Era)
+        table(hof$Era)
+        identical(summary(hof$Era),table(hof$Era)) # not identical
 
 T.Era <- table(hof$Era)
 T.Era
@@ -32,12 +43,15 @@ pie(table(hof$Era))
 
 # Section 3.3 Saving Graphs
 
-png("../output/bargraph.png")
+#png("../output/bargraph.png")
+        png("bargraph.png")
 barplot(table(hof$Era), xlab="Era", ylab="Frequency",
         main="Era of the Nonpitching Hall of Famers")
 dev.off()
 
-pdf("../output/graphs.pdf")
+#pdf("../output/graphs.pdf")
+pdf("graphs.pdf")
+
 barplot(table(hof$Era))
 plot(table(hof$Era))
 dev.off()
@@ -45,25 +59,28 @@ dev.off()
 # Section 2.4 Dot plots
 
 T.Era <- table(hof$Era)
+T.Era
 dotchart(as.numeric(T.Era), labels=names(T.Era), xlab="Frequency")
 
 hof.500 <- subset(hof, HR >= 500)
 hof.500 <- hof.500[order(hof.500$OPS), ]
+        hof.500
+
 dotchart(hof.500$OPS, labels=hof.500$X, xlab="OPS")
 
-# Section 2.5 Numeric Variable: Stripchart and Histogram
-
+# Section 3.5 Numeric Variable: Stripchart and Histogram
 windows(width=7, height=3.5)
 stripchart(hof$MidCareer, method="jitter", pch=1, 
            xlab="Mid Career")
-dev.off()
+#dev.off()
 
 hist(hof$MidCareer, xlab="Mid Career", main="")
 
 hist(hof$MidCareer, xlab="Mid Career", main="",
      breaks=seq(1880, 2000, by=20))
 
-# Section 2.6 Two Numeric Variables
+# Section 3.6 Two Numeric Variables
+# Section 3.6.1 Scatterplot
 
 with(hof, plot(MidCareer, OPS))
 with(hof, lines(lowess(MidCareer, OPS, f=0.3)))
@@ -97,6 +114,7 @@ with(hof, identify(OBP, SLG, X, n=6))
 
 hof$HR.Rate <- with(hof, HR / AB)
 
+# Section 3.7.1 Parallel stripcharts
 stripchart(HR.Rate ~ Era, data=hof)
 
 par(plt = c(.2, .94, .145, .883))
@@ -109,7 +127,9 @@ boxplot(HR.Rate ~ Era, data=hof, las=2,
 
 # Section 3.8 Comparing Ruth, Aaron, Bonds, and A-Rod
 
-master <- read.csv("lahman/Master.csv")
+#master <- read.csv("lahman/Master.csv")
+master <- read.csv("Master.csv")
+head(master)
 
 getinfo <- function(firstname, lastname){
         playerline <- subset(master,
@@ -119,20 +139,22 @@ getinfo <- function(firstname, lastname){
         birthmonth <- playerline$birthMonth
         birthday <- playerline$birthDay
         byear <- ifelse(birthmonth <= 6, birthyear, birthyear + 1)
-        list(name.code=name.code, byear=byear)}
+        list(name.code=name.code, byear=byear)
+        }
 
 ruth.info <- getinfo("Babe", "Ruth")
 aaron.info <- getinfo("Hank", "Aaron")
 bonds.info <- getinfo("Barry", "Bonds")
 arod.info <- getinfo("Alex", "Rodriguez")
 ruth.info
-
-batting <- read.csv("lahman/Batting.csv")
+batting <- read.csv("Batting.csv")
 
 ruth.data <- subset(batting, playerID == ruth.info$name.code)
 ruth.data$Age <- ruth.data$yearID - ruth.info$byear
+ruth.data
 
 aaron.data <- subset(batting, playerID == aaron.info$name.code)
+aaron.data
 aaron.data$Age <- aaron.data$yearID - aaron.info$byear
 bonds.data <- subset(batting, playerID == bonds.info$name.code)
 bonds.data$Age <- bonds.data$yearID - bonds.info$byear
@@ -152,19 +174,29 @@ legend(20, 700, legend=c("Bonds", "Aaron", "Ruth", "ARod"),
 
 # Section 3.9 The 1998 Home Run Race
 
-data1998 <- read.csv("all1998.csv", header=FALSE)
-fields <- read.csv("fields.csv")
-names(data1998) <- fields[, "Header"]
+data1998 <- read.csv("all1998.csv", header=TRUE)
+#fields <- read.csv("fields.csv")
+#names(data1998) <- fields[, "Header"]
 
 retro.ids <- read.csv("retrosheetIDs.csv")
+        head(retro.ids)
+
 sosa.id <- as.character(subset(retro.ids,
                                FIRST=="Sammy" & LAST=="Sosa")$ID)
+        sosa.id
 
 mac.id <- as.character(subset(retro.ids,
                               FIRST=="Mark" & LAST=="McGwire")$ID)
+        alf.id <- as.character(subset(retro.ids,
+                                      FIRST=="Edgardo" & LAST=="Alfonzo")$ID)
+
+ 
 
 sosa.data <- subset(data1998, BAT_ID == sosa.id)
+head(sosa.data)
+        names(sosa.data)
 mac.data <- subset(data1998, BAT_ID == mac.id)
+        alf.data <- subset(data1998, BAT_ID == alf.id)
 
 createdata <- function(d){
         d$Date <- as.Date(substr(d$GAME_ID, 4, 11),
@@ -177,10 +209,13 @@ createdata <- function(d){
 
 mac.hr <- createdata(mac.data)
 sosa.hr <- createdata(sosa.data)
-head(sosa.hr)
+        alf.hr <- createdata(alf.data)
 
+head(sosa.hr)
+        sosa.hr
 plot(mac.hr, type="l", lwd=2, ylab="Home Runs in the Season")
 lines(sosa.hr, lwd=2, col="grey")
+        lines(alf.hr, lwd=2, col="red")
 abline(h=62, lty=3)
 text(10440, 65, "62")
 legend(10440, 20, legend=c("McGwire (70)", "Sosa (66)"),
